b1a2c42385329aeb092e535f812d44df10e3c044
Bruno Paz <brunopaz@sapo.pt>
2019-06-22 15:44:52 +0100

    Update to Python 3 and Ulauncher API v2

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# ulauncher-statuspages
+# Ulauncher Status Pages
+
+[![Ulauncher Extension](https://img.shields.io/badge/Ulauncher-Extension-green.svg?style=for-the-badge)](https://ext.ulauncher.io/-/github-brpaz-ulauncher-statuspages)
+[![CircleCI](https://img.shields.io/circleci/build/github/brpaz/ulauncher-statuspages.svg?style=for-the-badge)](https://circleci.com/gh/brpaz/ulauncher-statuspages)
+![License](https://img.shields.io/github/license/brpaz/ulauncher-statuspages.svg?style=for-the-badge)
 
 > Ulauncher Extension that provides shortcuts for accessing status pages for popular services like GitHub, AWS and more.
 
@@ -8,9 +12,9 @@
 
 ## Requirements
 
-* [ulauncher](https://ulauncher.io/)
-* Python >= 2.7
-
+* [ulauncher 5](https://ulauncher.io/)
+* Python 3
+*
 ## Install
 
 Open ulauncher preferences window -> extensions -> add extension and paste the following url:
@@ -36,6 +40,7 @@ The following services are included by default:
 * NewRelic
 * Sendgrid
 * TravisCI
+* Discord
 
 Feel free to create a PR to add more services.
 
@@ -43,11 +48,25 @@ Feel free to create a PR to add more services.
 ## Development
 
 ```
+git clone https://github.com/brpaz/ulauncher-statuspages
 make link
 ```
 
-To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
+The `make link` command will symlink the cloned repo into the appropriate location on the ulauncher extensions folder.
+
+To see your changes, stop ulauncher and run it from the command line with: `ulauncher -v`.
+
+## Contributing
+
+Contributions, issues and Features requests are welcome.
+
+## Show your support
+
+<a href="https://www.buymeacoffee.com/Z1Bu6asGV" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
 
 ## License
 
-MIT
+Copywright @ 2019 [Bruno Paz](https://github.com/brpaz)
+
+This project is [MIT](LLICENSE) Licensed.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import logging
 import json
 import os
+from threading import Timer
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
@@ -10,7 +11,7 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.config import EXT_PREFERENCES_DIR
-from threading import Timer
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -34,19 +35,20 @@ class StatusPagesExtension(Extension):
 
         self.pages = []
 
-        if (os.path.isfile(os.path.join(EXT_PREFERENCES_DIR, 'status_pages.json'))):
+        if (os.path.isfile(
+                os.path.join(EXT_PREFERENCES_DIR, 'status_pages.json'))):
             LOGGER.info('Loading status pages from user')
             config = os.path.join(EXT_PREFERENCES_DIR, 'status_pages.json')
         else:
             LOGGER.info('Loading status pages from extension')
-            config = os.path.join(os.path.dirname(
-                __file__), 'data', 'pages.json')
+            config = os.path.join(os.path.dirname(__file__), 'data',
+                                  'pages.json')
 
         with open(config) as f:  # pylint: disable=invalid-name
             self.pages = json.load(f)
 
         # refresh pages every hour
-        t = Timer(3600, self.load_pages)
+        t = Timer(3600, self.load_pages)  # pylint: disable=invalid-name
         t.start()
 
 
@@ -59,17 +61,18 @@ class KeywordQueryEventListener(EventListener):
         pages = extension.pages
         query = event.get_argument()
         if query:
-            pages = [p for p in pages if query.lower()
-                     in p['label'].lower()]
+            pages = [p for p in pages if query.lower() in p['label'].lower()]
 
         pages = sorted(pages, key=lambda k: k['label'])
 
         items = []
         for page in pages[:8]:
-            items.append(ExtensionResultItem(icon=page['icon'],
-                                             name=page['label'],
-                                             description="Click to open the service status page",
-                                             on_enter=OpenUrlAction(page['url'])))
+            items.append(
+                ExtensionResultItem(
+                    icon=page['icon'],
+                    name=page['label'],
+                    description="Click to open the service status page",
+                    on_enter=OpenUrlAction(page['url'])))
 
         return RenderResultListAction(items)
 
